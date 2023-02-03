7c72f1c82dfb29c0d57f36d1709956b62735d0e3
skiddo <30941141+coolst3r@users.noreply.github.com>
2023-02-02 21:02:07 -0600

    Update antiaim.cpp

--- src/core/features/antiaim.cpp
+++ src/core/features/antiaim.cpp
@@ -1,93 +1,89 @@
-#include "../../includes.hpp"
-#include "features.hpp"
-
-bool updatingLby() {
-    // cred: Somewhere from UC, can't remember
-    AnimState* animState = Globals::localPlayer->animState();
-    float curtime = Globals::localPlayer->tickbase() * Interfaces::globals->interval_per_tick;
-    static float lbyTime;
-     
-    if (animState->verticalVelocity > 0.1f || fabs(animState->horizontalVelocity) > 100.0f) {
-        lbyTime = curtime + 0.22f;
-        return false;
-    }
-    else if (curtime > lbyTime) {
-        lbyTime = curtime + 1.1f;
-        return true;
-    }
-    return false;
-}
-
 void Features::AntiAim::createMove(CUserCmd* cmd) {
-    if (Interfaces::engine->IsInGame()) {
-        if (Globals::localPlayer) {
-            if (Globals::localPlayer->health() > 0 && Globals::localPlayer->moveType() != 9) { // 9 is ladder movetype
-                // TODO: Only turn off anti-aim for the throw time so it's more reliable.
-                Weapon *weapon = (Weapon *) Interfaces::entityList->GetClientEntity((uintptr_t) Globals::localPlayer->activeWeapon() & 0xFFF);
-                if(weapon) {
-                    if (!(weapon->itemIndex() == ItemIndex::WEAPON_DECOY || weapon->itemIndex() == ItemIndex::WEAPON_HEGRENADE || weapon->itemIndex() == ItemIndex::WEAPON_FLASHBANG || weapon->itemIndex() == ItemIndex::WEAPON_SMOKEGRENADE || weapon->itemIndex() == ItemIndex::WEAPON_MOLOTOV || weapon->itemIndex() == ItemIndex::WEAPON_INCGRENADE)) {
-                        if (!((cmd->buttons & (1 << 0)) || (cmd->buttons & (1 << 5)))) {
-                            if (CONFIGINT("Rage>AntiAim>Type")) {
-                                // TODO: for some reason it refuses to desync when looking forward???!?!?
-                                
-                                cmd->viewangles.x = CONFIGINT("Rage>AntiAim>Pitch");
-
-                                int real = 0;
-                                int fake = 0;
-
-                                switch (CONFIGINT("Rage>AntiAim>Type")) {
-                                    case 1: { // Static 
-                                        real = cmd->viewangles.y + CONFIGINT("Rage>AntiAim>Offset");
-                                        fake = CONFIGINT("Rage>AntiAim>Static>Desync");
-                                        break;
-                                    }
-                                    case 2: { // Jitter
-                                        int jitterAmt = (((cmd->tick_count % CONFIGINT("Rage>AntiAim>Jitter>Jitter Delay")) < CONFIGINT("Rage>AntiAim>Jitter>Jitter Delay") / 2) ? 1 : -1) * (CONFIGINT("Rage>AntiAim>Jitter>Jitter Amount") - (CONFIGINT("Rage>AntiAim>Jitter>Jitter Amount")/2));
-                                        real = cmd->viewangles.y + CONFIGINT("Rage>AntiAim>Offset") + jitterAmt;
-                                        fake = CONFIGINT("Rage>AntiAim>Jitter>Desync");
-                                        break;
-                                    }
-                                    case 3: { // Fake Jitter
-                                        int jitterAmt = (((cmd->tick_count % CONFIGINT("Rage>AntiAim>Fake Jitter>Jitter Delay")) < CONFIGINT("Rage>AntiAim>Fake Jitter>Jitter Delay") / 2) ? 1 : -1) * (CONFIGINT("Rage>AntiAim>Fake Jitter>Jitter Amount") - (CONFIGINT("Rage>AntiAim>Fake Jitter>Jitter Amount")/2));
-                                        real = cmd->viewangles.y + CONFIGINT("Rage>AntiAim>Offset");
-                                        fake = jitterAmt;
-                                        break;
-                                    }
-                                    case 4: { // Real Jitter 
-                                        int jitterAmt = (((cmd->tick_count % CONFIGINT("Rage>AntiAim>Real Jitter>Jitter Delay")) < CONFIGINT("Rage>AntiAim>Real Jitter>Jitter Delay") / 2) ? 1 : -1) * (CONFIGINT("Rage>AntiAim>Real Jitter>Jitter Amount") - (CONFIGINT("Rage>AntiAim>Real Jitter>Jitter Amount")/2));
-                                        real = cmd->viewangles.y + CONFIGINT("Rage>AntiAim>Offset") + jitterAmt;
-                                        fake = CONFIGINT("Rage>AntiAim>Static>Desync") - jitterAmt;
-                                        break;
-                                    }
-                                    case 5: { // Spin 
-                                        real = cmd->viewangles.y + (cmd->tick_count * CONFIGINT("Rage>AntiAim>Offset"));
-                                        fake = CONFIGINT("Rage>AntiAim>Spin>Desync");
-                                        break;
-                                    }
-                                }
-
-                                //TODO Check for net channel group 9 so we can desync and yell at nn's at the same time
-                                int fakelag = CONFIGINT("Rage>AntiAim>FakeLag");
-                                if (Interfaces::engine->IsVoiceRecording()) {
-                                    fakelag = 0;
-                                } else {
-                                    fakelag = CONFIGINT("Rage>AntiAim>FakeLag");
-                                }
-
-                                *Globals::sendPacket = cmd->tick_count % ((CONFIGBOOL("Rage>Enabled") && CONFIGINT("Rage>AntiAim>Type")) + fakelag + 1);
-
-                                if (updatingLby()) {
-                                    cmd->viewangles.y = real + (fake * 2);
-                                    *Globals::sendPacket = false;
-                                } else if (*Globals::sendPacket) {
-                                    cmd->viewangles.y = real;
-                                    fakeYaw = real;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+    if (!Interfaces::engine->IsInGame()) {
+        return;
+    }
+ 
+    if (!Globals::localPlayer || Globals::localPlayer->health() <= 0 || Globals::localPlayer->moveType() == 9) {
+        // Player is dead or on a ladder, don't use anti-aim.
+        return;
+    }
+ 
+    Weapon *weapon = (Weapon *) Interfaces::entityList->GetClientEntity((uintptr_t) Globals::localPlayer->activeWeapon() & 0xFFF);
+    if(!weapon) {
+        return;
+    }
+ 
+    if (weapon->itemIndex() == ItemIndex::WEAPON_DECOY || weapon->itemIndex() == ItemIndex::WEAPON_HEGRENADE || weapon->itemIndex() == ItemIndex::WEAPON_FLASHBANG || weapon->itemIndex() == ItemIndex::WEAPON_SMOKEGRENADE || weapon->itemIndex() == ItemIndex::WEAPON_MOLOTOV || weapon->itemIndex() == ItemIndex::WEAPON_INCGRENADE) {
+        // Player is using a throwable, don't use anti-aim.
+        return;
+    }
+ 
+    if ((cmd->buttons & (1 << 0)) || (cmd->buttons & (1 << 5))) {
+        // Player is shooting, don't use anti-aim.
+        return;
+    }
+ 
+    cmd->viewangles.x = CONFIGINT("Rage>AntiAim>Pitch");
+ 
+    int real = 0;
+    int fake = 0;
+ 
+    int jitterDelay = CONFIGINT("Rage>AntiAim>Jitter>Jitter Delay");
+    int jitterAmount = CONFIGINT("Rage>AntiAim>Jitter>Jitter Amount");
+    int fakeJitterDelay = CONFIGINT("Rage>AntiAim>Fake Jitter>Jitter Delay");
+    int fakeJitterAmount = CONFIGINT("Rage>AntiAim>Fake Jitter>Jitter Amount");
+    int realJitterDelay = CONFIGINT("Rage>AntiAim>Real Jitter>Jitter Delay");
+    int realJitterAmount = CONFIGINT("Rage>AntiAim>Real Jitter>Jitter Amount");
+    int lbyDelay = CONFIGINT("Rage>AntiAim>LBY>LBY Delay");
+    int lbyAmount = CONFIGINT("Rage>AntiAim>LBY>LBY Amount");
+    int spinAmount = CONFIGINT("Rage>AntiAim>Spin>Spin Amount");
+    int spinDelay = CONFIGINT("Rage>AntiAim>Spin>Spin Delay");
+ 
+    switch (CONFIGINT("Rage>AntiAim>Type")) {
+        case 1: { // Static
+            real = cmd->viewangles.y + CONFIGINT("Rage>AntiAim>Offset");
+            fake = CONFIGINT("Rage>AntiAim>Static>Desync");
+            break;
         }
+      case 2: { // Jitter
+            int jitterAmt = (((cmd->tick_count % jitterDelay) < jitterDelay / 2) ? 1 : -1) * (jitterAmount - (jitterAmount/2));
+            real = cmd->viewangles.y + CONFIGINT("Rage>AntiAim>Offset") + jitterAmt;
+            fake = CONFIGINT("Rage>AntiAim>Jitter>Desync");
+            break;
+        }
+        case 3: { // Fake Jitter
+            int jitterAmt = (((cmd->tick_count % fakeJitterDelay) < fakeJitterDelay / 2) ? 1 : -1) * (fakeJitterAmount - (fakeJitterAmount/2));
+            real = cmd->viewangles.y + CONFIGINT("Rage>AntiAim>Offset");
+            fake = jitterAmt;
+            break;
+        }
+        case 4: { // Real Jitter
+            int jitterAmt = (((cmd->tick_count % realJitterDelay) < realJitterDelay / 2) ? 1 : -1) * (realJitterAmount - (realJitterAmount/2));
+            real = cmd->viewangles.y + CONFIGINT("Rage>AntiAim>Offset") + jitterAmt;
+            fake = CONFIGINT("Rage>AntiAim>Real Jitter>Desync");
+            break;
+        }
+        case 5: { // LBY
+            if (updatingLby()) {
+                real = cmd->viewangles.y + CONFIGINT("Rage>AntiAim>Offset") + lbyAmount;
+            } else {
+                real = cmd->viewangles.y + CONFIGINT("Rage>AntiAim>Offset");
+            }
+            fake = CONFIGINT("Rage>AntiAim>LBY>Desync");
+            break;
+        }
+        case 6: { // Spin
+            int spinAmt = (((cmd->tick_count % spinDelay) < spinDelay / 2) ? 1 : -1) * (spinAmount - (spinAmount/2));
+            real = cmd->viewangles.y + CONFIGINT("Rage>AntiAim>Offset") + spinAmt;
+            fake = CONFIGINT("Rage>AntiAim>Spin>Desync");
+            break;
+        }
+    }
+ 
+    if (CONFIGINT("Rage>AntiAim>Fake>Method") == 1) {
+        cmd->viewangles.y = real;
+        cmd->viewangles.y += 180;
+    } else {
+        cmd->viewangles.y = fake;
     }
 }
